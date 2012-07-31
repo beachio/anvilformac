e84da1acf3869e93ab8c8a7530c53f7c65a571e3
Elliott Kember <elliott.kember@gmail.com>
2012-07-31 12:10:31 +0100

    Stubbed out data source files

---
+++ Anvil/NVDataSource.h
@@ -0,0 +1,13 @@
+//
+//  NVDataSource.h
+//  Anvil
+//
+//  Created by Elliott Kember on 31/07/2012.
+//  Copyright (c) 2012 Riot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NVDataSource : NSObject
+
+@end
---
+++ Anvil/NVDataSource.m
@@ -0,0 +1,13 @@
+//
+//  NVDataSource.m
+//  Anvil
+//
+//  Created by Elliott Kember on 31/07/2012.
+//  Copyright (c) 2012 Riot. All rights reserved.
+//
+
+#import "NVDataSource.h"
+
+@implementation NVDataSource
+
+@end
